818c3dafec1e7228e9070ce2716ece1f91cf4341
Maxcs318 <maxkiller1112@gmail.com>
2019-05-28 16:44:03 +0700

    respown
    --

--- ConnData/sessionForMaster.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // session_start(); 
-    if($_SESSION["m_status"]=="master") {
-        
-    }else{
-        header("location:http://localhost/plant/login.php"); 
-        exit(0);
-    }
-?>
