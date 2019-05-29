0cbdece63f3e1a8f912c6efd1fb24d93df73ab00
Maxcs318 <maxkiller1112@gmail.com>
2019-05-29 15:54:48 +0700

    ++

--- Posts/post_list_person.php
+++ Posts/post_list_person.php
@@ -72,11 +72,11 @@
                     <?php
                     // output data of each row
                     $check_post1 = 'a';
-                    while ($row = $result->fetch_assoc()) {
+                    while ($row0 = $result->fetch_assoc()) {
                         ?>
                             <div class="col-lg-12 col-xs-12">
-                            <a class="float:bottom" href='post_selected.php?getPostID= <?php echo $row["p_id"]; ?>'>
-                                Post ID: <?php echo $row["p_id"]; ?> Have been Comments.
+                            <a class="float:bottom" href='post_selected.php?getPostID= <?php echo $row0["p_id"]; ?>'>
+                                Post ID: <?php echo $row0["p_id"]; ?> Have been Comments.
                             </a>
                             </div> 
                         <?php
@@ -103,10 +103,10 @@
                     <div class="row">
                     <?php
                     // output data of each row
-                    while ($row = $result->fetch_assoc()) {
+                    while ($row1 = $result->fetch_assoc()) {
                         ?>
                             <div class="col-lg-12 col-xs-12">
-                            <!-- <a class="float:bottom" href='post_selected.php?getPostID= <?php echo $row["cl_id"]; ?>'>View Post</a> -->
+                            <!-- <a class="float:bottom" href='post_selected.php?getPostID= <?php echo $row1["cl_id"]; ?>'>View Post</a> -->
                                 Classification : ID <?php echo $row['cl_id']; ?> Have been Confirm.
                             </div>                        
                         <?php
