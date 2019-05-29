cb143eef215bfafcf386a3d2cad20fd11c0e5fd7
Wannapon Rotjanarungtawee <40911600+Wannapon@users.noreply.github.com>
2019-05-29 16:02:53 +0700

    <p> slide shake

--- Aboutplant/Disease.php
+++ Aboutplant/Disease.php
@@ -24,14 +24,14 @@
 
     </div>
     <!-- slide text -->
-    <div class="row" style="margin-bottom: 70px;">
+    <div class="row">
         <p class="item-1 ">EXPERT SYSTEM FOR PLANT DISEASE CLASSIFICATION [item-1]</p>
         <p class="item-2 ">Some Text for [item-2]</p>
         <p class="item-3 ">Some Text for [item-3]</p>
     </div>
     <!-- end slide text -->
 
-    <div class="container">
+    <div class="container" style="margin-top: 50px;">
         <!-- home button -->
         <div class="col-xs-4 col-md-4">
 
--- Classification/classification_list_person.php
+++ Classification/classification_list_person.php
@@ -29,7 +29,7 @@ error_reporting(E_ALL ^ E_NOTICE); ?>
     <!--end user id top -->
 
     <!-- slide text -->
-    <div class="row" style="margin-bottom: 100px;">
+    <div class="row">
         <p class="item-1 ">EXPERT SYSTEM FOR PLANT DISEASE CLASSIFICATION [item-1]</p>
         <p class="item-2 ">Some Text for [item-2]</p>
         <p class="item-3 ">Some Text for [item-3]</p>
