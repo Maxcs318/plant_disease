d6e59ba67d58744bc17429523b7f74e958f73a3d
Maxcs318 <maxkiller1112@gmail.com>
2019-05-31 10:04:08 +0700

    ++

--- Actor/Admin/AdminPage.php
+++ Actor/Admin/AdminPage.php
@@ -31,7 +31,7 @@
     <!--end user id top -->
 
       <!-- slide text -->
-      <div class="row" style="margin-bottom: 50px;">
+      <div class="row">
         <p class="item-1 ">EXPERT SYSTEM FOR PLANT DISEASE CLASSIFICATION [item-1]</p>
         <p class="item-2 ">Some Text for [item-2]</p>
         <p class="item-3 ">Some Text for [item-3]</p>
