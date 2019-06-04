dac265410e51d48cd6425efc05cef55542d7cc26
Maxcs318 <maxkiller1112@gmail.com>
2019-06-04 12:16:22 +0700

    can login by email , username , phone number
    ++

--- ConnData/check_login.php
+++ ConnData/check_login.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-require_once("connectDB.php");
-
 if(isset($_SESSION["m_status"])){
     if($_SESSION["m_status"] == "admin"){
         header("location:../Actor/Admin/AdminPage.php");
@@ -14,25 +12,12 @@ if(isset($_SESSION["m_status"])){
 
 $strUsername = $_POST['username'];
 $strPassword = $_POST['password'];
-
+require_once("connectDB.php");
 $sql = "SELECT * FROM member WHERE m_username = '".$strUsername."' and m_password = '".$strPassword."'";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 
-	if ($result->num_rows == 0) {
-		?>
-		<!DOCTYPE html>
-		<html>
-		<head>    
-    	</head>
-		<body onload="setTimeout(function() { document.frm1.submit() },10)">
-            <form action="../login.php" name="frm1" method="POST">
-                <input type="hidden" name="checkLogin" value="Username or Password is Incorrect" />
-            </form>
-        </body>
-		</html>
-	<?php	
-	}else{	
+	if ($result->num_rows != 0) {
 		$_SESSION["m_id"] = $row["m_id"];
 		$_SESSION["m_firstname"] = $row["m_firstname"];
 		$_SESSION["m_lastname"] = $row["m_lastname"];
@@ -51,8 +36,80 @@ $row = $result->fetch_assoc();
 		}else if($row["m_status"] == "user") {
 			header("location:../Actor/User/UserPage.php");
 		}
-			
+		$conn->close();
+	}else{	
+		
+		require_once("connectDB.php");
+		$sql = "SELECT * FROM member WHERE m_phone = '".$strUsername."' and m_password = '".$strPassword."'";
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		if ($result->num_rows != 0) {
+			$_SESSION["m_id"] = $row["m_id"];
+			$_SESSION["m_firstname"] = $row["m_firstname"];
+			$_SESSION["m_lastname"] = $row["m_lastname"];
+			// $_SESSION["m_email"] = $row["m_email"];
+			// $_SESSION["m_phone"] = $row["m_phone"];
+			$_SESSION["m_status"] = $row["m_status"];
+			$_SESSION["m_username"] = $row["m_username"];
+			$_SESSION["m_imageprofile"] = $row["m_imageprofile"];  
+	
+			//Bla Bla Bla
+			session_write_close();
+			if($row["m_status"] == "admin"){
+				header("location:../Actor/Admin/AdminPage.php");
+			}else if($row["m_status"] == "expert"){
+				header("location:../Actor/Expert/ExpertPage.php");
+			}else if($row["m_status"] == "user") {
+				header("location:../Actor/User/UserPage.php");
+			}
+			$conn->close();
+		}else{ 
+			require_once("connectDB.php");
+			$sql = "SELECT * FROM member WHERE m_email = '".$strUsername."' and m_password = '".$strPassword."'";
+			$result = $conn->query($sql);
+			$row = $result->fetch_assoc();
+			if ($result->num_rows != 0) {
+				$_SESSION["m_id"] = $row["m_id"];
+				$_SESSION["m_firstname"] = $row["m_firstname"];
+				$_SESSION["m_lastname"] = $row["m_lastname"];
+				// $_SESSION["m_email"] = $row["m_email"];
+				// $_SESSION["m_phone"] = $row["m_phone"];
+				$_SESSION["m_status"] = $row["m_status"];
+				$_SESSION["m_username"] = $row["m_username"];
+				$_SESSION["m_imageprofile"] = $row["m_imageprofile"];  
+		
+				//Bla Bla Bla
+				session_write_close();
+				if($row["m_status"] == "admin"){
+					header("location:../Actor/Admin/AdminPage.php");
+				}else if($row["m_status"] == "expert"){
+					header("location:../Actor/Expert/ExpertPage.php");
+				}else if($row["m_status"] == "user") {
+					header("location:../Actor/User/UserPage.php");
+				}
+				$conn->close();
+			}else{ 
+				?>
+				<!DOCTYPE html>
+				<html>
+				<head>    
+				</head>
+				<body onload="setTimeout(function() { document.frm1.submit() },10)">
+					<form action="../login.php" name="frm1" method="POST">
+						<input type="hidden" name="checkLogin" value="Username or Password is Incorrect" />
+					</form>
+				</body>
+				</html>
+				<?php	
+
+			}
+
+		}
+		
+
 	}
-$conn->close();
+
     
 ?>
+
+
