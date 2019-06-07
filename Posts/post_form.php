5b3add40fb519d4a5fcad4d60b40d4e01d97f740
Wannapon Rotjanarungtawee <40911600+Wannapon@users.noreply.github.com>
2019-06-07 15:39:38 +0700

    post form
    ++ maxlength header 50 , detail 100

--- Posts/post_form.php
+++ Posts/post_form.php
@@ -42,9 +42,9 @@
 
                     <h1 class="header">Create Post</h1>
                     <label id="first">Header :</label><br />
-                    <input class="form-control" type="text" name="header" maxlength="100" style="width:40%;" required><br />
+                    <input class="form-control" type="text" name="header" maxlength="50" style="width:40%;" required><br />
                     <label id="first">Detail :</label><br />
-                    <textarea class="form-control" rows="5" type="text" name="detail" required></textarea>
+                    <textarea class="form-control" rows="5" type="text" maxlength="100" name="detail" required></textarea>
                     <br>
                     <br>
 
