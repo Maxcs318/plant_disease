0bbe82a6b8f1390024c5a8d6113713da432edf47
Wannapon Rotjanarungtawee <40911600+Wannapon@users.noreply.github.com>
2019-06-18 10:20:55 +0700

    Update connectDB.php
    set UTF-8

--- ConnData/connectDB.php
+++ ConnData/connectDB.php
@@ -5,6 +5,7 @@
     $dbname = "plant_project";
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset("utf8");
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
